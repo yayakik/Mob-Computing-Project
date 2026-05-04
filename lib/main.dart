2721f4c1a76bce9e68a43efe7c4689c1d2ec5a2e
PierreGattuso <piro.wadie@gmail.com>
2026-05-04 16:30:07 +0300

    Add design files

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie1/cubit/auth_cubit.dart';
+import 'package:movie1/cubit/movie_cubit.dart';
+import 'package:movie1/cubit/myListCubit.dart';
+import 'package:movie1/firebase_options.dart';
+import 'package:movie1/view/login_signup/entry.dart';
+import 'package:movie1/cubit/password_cubit.dart';
+import 'view/home/Home.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => MoviesCubit()),
+        BlocProvider(create: (context) => MyListCubit()),
+        BlocProvider(create: (context) => PasswordCubit()),
+        BlocProvider(create: (context) => AuthCubit()),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(useMaterial3: true),
+        debugShowCheckedModeBanner: false,
+        home: FirebaseAuth.instance.currentUser == null
+            ? const EntryScrean()
+            : Home(),
+        routes: {
+          "entry": (BuildContext context) => const EntryScrean(),
+          "home": (BuildContext context) => Home(),
+        },
+      ),
+    );
+  }
+}
